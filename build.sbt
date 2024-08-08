d95b4e5a5ef1d9cf82d05e7ffaa137de9263fe25
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-08-08 03:59:14 +0000

    simple hello get handler implemented

--- build.sbt
+++ build.sbt
@@ -1,1 +1,16 @@
-name := "scala-playground"
+name := "scala-rest-api-generator"
+version := "0.1"
+scalaVersion := "2.13.14"
+
+val AkkaVersion = "2.9.4"
+val AkkaHttpVersion = "10.6.3"
+
+resolvers += "Akka library repository".at("https://repo.akka.io/maven")
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
+  "com.typesafe.akka" %% "akka-pki" % AkkaVersion
+)
--- main.scala
+++ main.scala
@@ -1,16 +1,36 @@
-/* Welcome to the Scala Playground!
- *
- * This repl utilizes the `sbtn` thin client.
- *
- * To increase compilation speed, it's recommended to
- * start `sbt` in a Shell tab before clicking "Run".
- *
- * This will use a single instance of sbt for all
- * compilation, instead of launching sbt every time.
- */
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+
+import scala.io.StdIn
 
 object Main {
+  // Case class for our API response
+  case class ApiResponse(message: String)
+
+  // JSON formatting for our ApiResponse
+  implicit val apiResponseFormat = jsonFormat1(ApiResponse)
+  
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    implicit val system = ActorSystem(Behaviors.empty, "scala-rest-api-generator")
+    implicit val executionContext = system.executionContext
+
+    val route =
+      path("hello") {
+        get {
+          complete(ApiResponse("Hello, World!"))
+        }
+      }
+
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    println("Server online at http://localhost:8080/")
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
   }
 }
